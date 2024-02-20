8fa0685d458baea53a1229ddc5c940e1e16f1fde
Yulya Govorova <tishina1512@mail.ru>
2024-02-20 21:09:13 +1100

    added migrate?

--- users/models.py
+++ users/models.py
@@ -9,9 +9,9 @@ from users.utils import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
-    phone = models.CharField(max_length=20, verbose_name='телефон')
-    city = models.CharField(max_length=50, verbose_name='город')
-    country = models.CharField(max_length=50, verbose_name='страна')
+    phone = models.CharField(max_length=20, verbose_name='телефон', **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
 
     USERNAME_FIELD = "email"
