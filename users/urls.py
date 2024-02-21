0f2f67971f2fe67c6f42a38ac667de654b2de2c6
Yulya Govorova <tishina1512@mail.ru>
2024-02-21 18:25:18 +1100

    change commands

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
 
+
+
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/create/', UserCreateAPIView.as_view(), name='user_create'),
     path('user/list/', UserListAPIView.as_view(), name='user_list'),
     path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_get'),
     path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
     path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
+
 ]
